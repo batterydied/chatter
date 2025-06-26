2cabdb7a6b850b164d4f9bc6c5c1a0cabd9ba195
Benson Zheng <bensonzheng2003@gmail.com>
2025-06-25 21:29:41 -0400

    installed daisyui for frontend

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -2,7 +2,6 @@ import type { User } from 'firebase/auth'
 import { Navigate } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect } from 'react'
-
 type HomeProps = {
     user: User | null
     logOut: () => void,
@@ -11,12 +10,32 @@ type HomeProps = {
 
 const HomePage = ({user, logOut} : HomeProps) => {
     const fetchUserFromDB = async (email: string) => {
-        const res = await axios.get(import.meta.env.VITE_BACKEND_API_URL + `user/${email}`)
-        const data = res.data
-        if(data.status == 'Success'){
+        try{
+            const res = await axios.get(import.meta.env.VITE_BACKEND_API_URL + `user/${email}`)
+            const data = res.data
             console.log(data.user)
-        }else{
-            console.log('Failed to retrieve user')
+        }catch(e){
+            if(e instanceof Error){
+                console.log(e.message)
+            }else{
+                console.log('Unknown error occurred')
+            }
+
+            const body = {
+                name: email,
+                email
+            }
+            try{
+                const res = await axios.post(import.meta.env.VITE_BACKEND_API_URL + `user/`, body)
+                const data = res.data
+                console.log(data)
+            }catch(e){
+                if(e instanceof Error){
+                    console.log(e.message)
+                }else{
+                    console.log('Unknown error occurred')
+                }
+            }
         }
     }
     useEffect(()=>{
@@ -27,13 +46,32 @@ const HomePage = ({user, logOut} : HomeProps) => {
     return (
         <>
             {user ? 
-                <div>
-                    <p>
+                <div className='flex flex-row w-full h-full'>
+                    <div className='list w-1/3 h-5/6 overflow-y-auto'>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                        <text className='list-row'> hi</text>
+                    </div>
+                    <div className='w-2/3'>
+                        <p>
                         Welcome, {user.email}
-                    </p>
-                    <button onClick={logOut} >
-                        Log out
-                    </button>
+                        </p>
+                        <button onClick={logOut} >
+                            Log out
+                        </button>
+                    </div>
                 </div>
                 :
                 <Navigate to='/'/>
