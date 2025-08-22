bbe66187b0b5cc63235b6173ad7bd7cb554d34f6
Benson Zheng <bensonzheng2003@gmail.com>
2025-08-22 15:48:02 -0400

    updated backend config for vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
 import 'dotenv/config'
 import express from "express"
-import UserRoutes from "../src/routes/userRoutes.js"
-import RelationRoutes from "../src/routes/relationRoutes.js"
-import ConversationRoutes from "../src/routes/conversationRoutes.js"
+import UserRoutes from "./routes/userRoutes.js"
+import RelationRoutes from "./routes/relationRoutes.js"
+import ConversationRoutes from "./routes/conversationRoutes.js"
 import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const MODE = process.env.MODE
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: MODE !== 'production' ? 'http://localhost:5173' : FRONTEND_URL,
   credentials: true
 }))
 
@@ -24,10 +25,8 @@ app.get('/', (req, res) => {
     res.send({message: 'You have reached the server.'})
 })
 
-if(process.env.MODE !== 'production'){
-  app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-  })
-}
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
 export default app
