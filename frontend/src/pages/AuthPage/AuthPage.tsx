6fb0fa5edd8e9be3c7c4bccc9bd367d2ef13b34d
Benson Zheng <bensonzheng2003@gmail.com>
2025-08-27 13:40:29 -0400

    added condit ional rendering for login animation in case device doesn't support We bGL

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,33 +1,41 @@
-import { Navigate } from 'react-router-dom'
-import { useAppContext } from '../../hooks/useAppContext'
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../../hooks/useAppContext';
 import Spline from '@splinetool/react-spline';
 
-const AuthPage = () => {
-    const {user, logIn} = useAppContext()
-    return (
-        <div className='bg-black overflow-hidden flex justify-center items-center h-screen w-screen'>
-            {user ? 
-                <Navigate 
-                    to='/home'
-                />
-                :
-                <>  
-                    <div className='absolute animate-fade-in'>
-                        <div className='text-7xl font-bold hover:cursor-default'>
-                            Chatter
-                        </div>
-                        <button onClick={logIn} className='border p-2 rounded-md border-transparent hover:border-accent hover:cursor-pointer'>
-                            Sign in with Google
-                        </button>
-                    </div>
-                    <div className='w-full h-full'>
-                        <Spline scene="https://prod.spline.design/5DvbVl2fOfUp5M5R/scene.splinecode" />
-                    </div>
-                </>
-                
-            }
-        </div>
-    )
-}
+const isWebGLAvailable = (): boolean => {
+  try {
+    const canvas = document.createElement('canvas');
+    return !!(
+      window.WebGLRenderingContext &&
+      (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
+    );
+  } catch {
+    return false;
+  }
+};
 
-export default AuthPage
+const AuthPage = () => {
+  const { user, logIn } = useAppContext();
+  const canUseWebGL = isWebGLAvailable()
+
+  if (user) return <Navigate to="/home" />;
+
+  return (
+    <div className="bg-black overflow-hidden flex justify-center items-center h-screen w-screen relative">
+      <div className="absolute animate-fade-in flex flex-col items-center gap-4">
+        <div className="text-7xl font-bold hover:cursor-default">Chatter</div>
+        <button className="border p-2 rounded-md border-transparent hover:border-accent hover:cursor-pointer" onClick={logIn}>
+          Sign in with Google
+        </button>
+      </div>
+
+      <div className="w-full h-full">
+        {canUseWebGL && 
+          <Spline scene="https://prod.spline.design/5DvbVl2fOfUp5M5R/scene.splinecode" />
+        }
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
