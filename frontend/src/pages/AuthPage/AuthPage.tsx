79c5a4402b17a3eb096087cf27a839b79dfe944c
Benson Zheng <bensonzheng2003@gmail.com>
2025-08-27 13:44:53 -0400

    disabled overscroll for login animation

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -21,7 +21,7 @@ const AuthPage = () => {
   if (user) return <Navigate to="/home" />;
 
   return (
-    <div className="bg-black overflow-hidden flex justify-center items-center h-screen w-screen relative">
+    <div className="bg-black overscroll-none flex justify-center items-center h-screen w-screen relative">
       <div className="absolute animate-fade-in flex flex-col items-center gap-4">
         <div className="text-7xl font-bold hover:cursor-default">Chatter</div>
         <button className="border p-2 rounded-md border-transparent hover:border-accent hover:cursor-pointer" onClick={logIn}>
